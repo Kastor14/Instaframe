4286b42733837ea7fe31ee84a50b486b4bf41e4a
Jean-Baptiste Waring <jean.baptiste.waring@icloud.com>
2021-02-06 14:28:30 -0500

    Initial Commit

---
+++ Instaframe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instaframe
+//
+//  Created by Jean-Baptiste Waring on 2021-02-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
